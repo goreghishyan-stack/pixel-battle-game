a4bd7b968325aaa1540c951d28eddc20bef611c6
goreghishyan-stack <goreghishyan@gmail.com>
2026-04-12 10:46:38 +0400

    Update server.js

--- server.js
+++ server.js
@@ -12,22 +12,18 @@ io.on('connection', (socket) => {
 
     socket.on('setPixel', (data) => {
         const id = `${data.x}-${data.y}`;
-        
-        // Сохраняем пиксель
-        pixelData[id] = { color: data.color, user: data.user };
+        pixelData[id] = { color: data.color };
         io.emit('updatePixel', { x: data.x, y: data.y, color: data.color });
 
-        // Магия CS2: через 5 секунд удаляем этот пиксель
+        // Удаление через 5 секунд
         setTimeout(() => {
             if (pixelData[id]) {
                 delete pixelData[id];
                 io.emit('removePixel', { x: data.x, y: data.y });
             }
-        }, 5000); // 5000 миллисекунд = 5 секунд
+        }, 5000);
     });
 });
 
 const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-    console.log('Сервер запущен на порту ' + PORT);
-});
+http.listen(PORT, () => { console.log('Server is running'); });
